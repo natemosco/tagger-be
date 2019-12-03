6c4e8916020ce4e083100af5ac226ca48362ac7d
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-12-03 18:35:47 -0500

    added an endpoint to be able to post the token as a json object instead of having to use the token saved locally Co-authored-by: Marcus Jones <indelibleink84@gmail.com> William Parker <weparkerjr@gmail.com>

--- api/messages/message-router.js
+++ api/messages/message-router.js
@@ -21,6 +21,99 @@ const SCOPES = [
 
 const TOKEN_PATH = 'token.json';
 
+router.post('/postTags', (req, res) => {
+
+  console.log(req.body);
+  let token = req.body;
+
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Gmail API.
+    authorize(JSON.parse(content), listMessages);
+  });
+  
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+  */
+  
+  function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+  
+    // Check if we have previously stored a token.
+    // fs.readFile(TOKEN_PATH, (err, token) => {
+    //   if (err) return getNewToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(token);
+      callback(oAuth2Client);
+    // });
+  }
+  
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback for the authorized client.
+   */
+  // function getNewToken(oAuth2Client, callback) {
+  //   const authUrl = oAuth2Client.generateAuthUrl({
+  //     access_type: 'offline',
+  //     scope: SCOPES,
+  //   });
+  //   console.log('Authorize this app by visiting this url:', authUrl);
+  //   const rl = readline.createInterface({
+  //     input: process.stdin,
+  //     output: process.stdout,
+  //   });
+  //   rl.question('Enter the code from that page here: ', (code) => {
+  //     rl.close();
+  //     oAuth2Client.getToken(code, (err, token) => {
+  //       if (err) return console.error('Error retrieving access token', err);
+  //       oAuth2Client.setCredentials(token);
+  //       // Store the token to disk for later program executions
+  //       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+  //         if (err) return console.error(err);
+  //         console.log('Token stored to', TOKEN_PATH);
+  //         console.log(token);
+  //       });
+  //       callback(oAuth2Client);
+  //     });
+  //   });
+  // }
+  
+  function listMessages(auth) {
+    const gmail = google.gmail({version: 'v1', auth});
+    gmail.users.messages.list({
+      userId: 'me',
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const messages = res.data.messages;
+      if (messages.length) {
+        console.log('Message ID\'s:');
+        messages.forEach((message) => {
+          gmail.users.messages.get({
+            userId: 'me',
+            id: message.id,
+          }, (err, res) => {
+            // searches for messages with the specifc IDs that we found on the previous lines
+            // this if else statement prevents errors from the parts array returning undefined
+            if(res.data.payload.parts !== undefined) {
+              messageArray.push(Buffer.from(res.data.payload.parts[0].body.data, 'base64').toString());
+            } else {
+              return null;
+            }
+          })
+        });
+      } else {
+        console.log('No messages found.');
+      }
+    })
+  }
+})
+
 router.get('/getTags', (req, res) => {
   fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
