b67b26b3b04b9d3c29737a555c2e355a50101ecd
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-12-06 12:09:13 -0500

    cracked the issue weve had in regards to connecting FE to BE. all of the "communication steps" are working seperately but WIP still because we need to make the communcation happen in one HTTP request

--- api/messages/message-router.js
+++ api/messages/message-router.js
@@ -4,6 +4,147 @@ const router = require("express").Router();
 const axios = require("axios");
 // const auth = require('../auth/auth-router')
 
+
+// TEST GMAIL API CALL 2 *************
+
+const {google} = require('googleapis');
+const {OAuth2Client} = require('google-auth-library');
+const http = require('http');
+const url = require('url');
+const open = require('open');
+const destroyer = require('server-destroy');
+
+router.get('/authCodeTest', (req, res) => {
+
+  const oauth2Client = new OAuth2Client(
+    "394607797501-9g0r8lala9ubsh2l6krcp1ini3vkvi2v.apps.googleusercontent.com",
+    "Z1WFuJ9ezhmm8F8iV6ZgCCn3",
+    "http://localhost:3000/oauth2callback",
+    "authoization_code"
+  );
+  let code = '4/twFekyhbFcFe7sHItMmGN0Z6nYdyXBxDuN1LhyeToHap-BIVitV9LCRRh_ZcfTm-lYJb-djHXPE6RxaeY4Q7C3A'
+  
+  const {tokens} = oauth2Client.getToken(code)
+  console.log(tokens);
+  oauth2Client.setCredentials(tokens);
+})
+
+router.post('/authCodeTest2', (req, res) => {
+  
+  let code = req.body.code;
+  console.log("code", code);
+
+  axios.post('https://www.googleapis.com/oauth2/v4/token', {
+      code: code,
+      client_id: "498525641423-gv4h1poto9mdbdlj7qibo9sf0t4f2231.apps.googleusercontent.com",
+      client_secret: "AGBziX-GP5CKEc9vckgr28I8",
+      redirect_uri: "http://localhost:3000",
+      grant_type: "authorization_code"
+    })
+    .then((res) => {
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(res)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+})
+
+router.get('/testAuth2', (req, res) => {
+  /**
+   * Start by acquiring a pre-authenticated oAuth2 client.
+   */
+  async function main() {
+    const oAuth2Client = await getAuthenticatedClient();
+    // Make a simple request to the People API using our pre-authenticated client. The `request()` method
+    // takes an GaxiosOptions object.  Visit https://github.com/JustinBeckwith/gaxios.
+    const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
+    const res = await oAuth2Client.request({url});
+    console.log(res.data);
+
+    // After acquiring an access_token, you may want to check on the audience, expiration,
+    // or original scopes requested.  You can do that with the `getTokenInfo` method.
+    const tokenInfo = await oAuth2Client.getTokenInfo(
+      oAuth2Client.credentials.access_token
+    );
+    console.log(tokenInfo);
+  }
+
+  /**
+   * Create a new OAuth2Client, and go through the OAuth2 content
+   * workflow.  Return the full client to the callback.
+   */
+  function getAuthenticatedClient() {
+    return new Promise((resolve, reject) => {
+      // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
+      // which should be downloaded from the Google Developers Console.
+      const oAuth2Client = new OAuth2Client(
+        "394607797501-9g0r8lala9ubsh2l6krcp1ini3vkvi2v.apps.googleusercontent.com",
+        "Z1WFuJ9ezhmm8F8iV6ZgCCn3",
+        "http://localhost:3000/oauth2callback"
+      );
+
+      // Generate the url that will be used for the consent dialog.
+      const authorizeUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile',
+      });
+
+      // Open an http server to accept the oauth callback. In this simple example, the
+      // only request to our webserver is to /oauth2callback?code=<code>
+      const server = http
+        .createServer(async (req, res) => {
+          try {
+            if (req.url.indexOf('/oauth2callback') > -1) {
+              // acquire the code from the querystring, and close the web server.
+              const qs = new url.URL(req.url, 'http://localhost:3000')
+                .searchParams;
+              const code = qs.get('code');
+              console.log(`Code is ${code}`);
+              res.end('Authentication successful! Please return to the console.');
+              server.destroy();
+
+              // Now that we have the code, use that to acquire tokens.
+              const r = await oAuth2Client.getToken(code);
+              console.log(r.tokens);
+              // Make sure to set the credentials on the OAuth2 client.
+              oAuth2Client.setCredentials(r.tokens);
+              // console.info('Tokens acquired.');
+              resolve(oAuth2Client);
+            }
+          } catch (e) {
+            reject(e);
+          }
+        })
+        .listen(3000, () => {
+          // open the browser to the authorize url to start the workflow
+          open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+        });
+      destroyer(server);
+    });
+  }
+
+  main().catch(console.error);
+})
+
+
+
+
+// END TEST GMAIL API CALL 2 ****************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // TEST GMAIL API CALL ***********
 
 // global array variable that will hold all the messages as strings
@@ -12,7 +153,7 @@ let messageArray = [];
 require('dotenv').config();
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+// const {google} = require('googleapis');
 
 const SCOPES = [
   'https://www.googleapis.com/auth/gmail.readonly',
@@ -20,6 +161,30 @@ const SCOPES = [
 ]; 
 
 const TOKEN_PATH = 'token.json';
+
+router.post('/testAuth', (req, res) => {
+
+  console.log(req.body);
+  let id_token = req.body.token;
+  let CLIENT_ID = req.body.clientId;
+
+  const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: id_token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+  console.log(ticket);
+}
+verify().catch(console.error);
+})
 
 router.post('/postTags', (req, res) => {
 
@@ -42,7 +207,7 @@ router.post('/postTags', (req, res) => {
   function authorize(credentials, callback) {
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+        client_id, client_secret, redirect_uris[3], {"grant-type" : "authorization_code"});
   
     // Check if we have previously stored a token.
     // fs.readFile(TOKEN_PATH, (err, token) => {
@@ -186,6 +351,8 @@ router.get('/getTags', (req, res) => {
             userId: 'me',
             id: message.id,
           }, (err, res) => {
+            console.log(`\n ******************* \n ${res.data.id} \n ******************* \n`);
+            console.log(Buffer.from(res.data.payload.parts[0].body.data, 'base64').toString());
             // searches for messages with the specifc IDs that we found on the previous lines
             // this if else statement prevents errors from the parts array returning undefined
             if(res.data.payload.parts !== undefined) {
