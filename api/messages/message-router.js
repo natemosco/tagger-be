a11f3a0ab280c50431a16ac2cdf3a5216c87c149
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-01-07 18:39:54 -0500

    changing maxRPS to 1 per 2 seconds WIP

--- api/messages/message-router.js
+++ api/messages/message-router.js
@@ -10,7 +10,7 @@ const rateLimit = require('axios-rate-limit');
 // ******* GLOBAL VARIABLES **********
 let responseLabels = "";
 let messages = "";
-const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 5000 });
+const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 2000 });
 http.getMaxRPS();
 
 
