7532331ad8b8258651425b7803ccfc736a8ff694
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-02-04 12:24:47 -0600

    created IMAP route without tagging

--- api/messages/message-router.js
+++ api/messages/message-router.js
@@ -4,7 +4,8 @@ const axios = require("axios");
 const { google } = require("googleapis");
 require("dotenv").config();
 const rateLimit = require("axios-rate-limit");
-var Imap = require("imap");
+const Imap = require("imap");
+const inspect = require("util").inspect;
 const simpleParser = require("mailparser").simpleParser;
 const Users = require("../users/user-model");
 const Messages = require("./message-model");
@@ -19,9 +20,91 @@ const http = rateLimit(axios.create(), {
 });
 http.getMaxRPS();
 
-// ********* THE NEW ROUTE WITH IMAP ************
-route.post("/", (req, res) => {
+// ********* THE NEW ROUTE WITH IMAP EMAILS WITHOUT TAGS***********
+router.post("/", (req, res) => {
   const { email, host, token } = req.body;
+  const allMessages = [];
+
+  var imap = new Imap({
+    user: email,
+    password: "",
+    host: host,
+    port: 993,
+    tls: true,
+    xoauth2: token,
+    tlsOptions: { rejectUnauthorized: false },
+    debug: console.log
+  });
+
+  function openInbox(cb) {
+    imap.openBox("INBOX", true, cb);
+  }
+
+  imap.once("ready", function() {
+    openInbox(function(err, box) {
+      if (err) throw err;
+      imap.search(["ALL"], function(err, results) {
+        if (err) throw err;
+        var f = imap.fetch(results, { bodies: "" });
+        f.on("message", function(msg, seqno) {
+          console.log("Message #%d", seqno);
+          var prefix = "(#" + seqno + ") ";
+          msg.on("body", function(stream, info) {
+            console.log(prefix + "Body");
+            simpleParser(stream, { bodies: "" })
+              .then(parsed => {
+                let newObj = {
+                  html: parsed.html,
+                  text: parsed.text,
+                  from: parsed.from,
+                  subject: parsed.subject,
+                  attachments: parsed.attachments,
+                  id: parsed.messageId
+                };
+                allMessages.push(newObj);
+              })
+              .catch(err => {
+                console.log("\n\n\nerr: ", err);
+              });
+          });
+          msg.once("attributes", function(attrs) {
+            console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
+          });
+          msg.once("end", function() {
+            console.log(prefix + "Finished");
+          });
+        });
+        f.once("error", function(err) {
+          console.log("Fetch error: " + err);
+        });
+        f.once("end", function() {
+          console.log("Done fetching all messages!");
+          res.status(200).json(allMessages);
+          imap.end();
+        });
+        // res.status(200).json(allMessages);
+      });
+
+      // this is where we will do res.status() and return the allMessages array for the front end.
+      // the allMessages array is an object of emails.
+    });
+
+    imap.once("error", function(err) {
+      console.log(err);
+    });
+
+    imap.once("end", function() {
+      console.log("Connection ended");
+    });
+  });
+
+  imap.connect();
+});
+
+// ********* THE NEW ROUTE WITH IMAP FOR TAGGING************
+router.post("/tags", (req, res) => {
+  const { email, host, token } = req.body;
+  const allMessages = [];
 
   var imap = new Imap({
     user: email,
@@ -58,27 +141,29 @@ route.post("/", (req, res) => {
   imap.once("ready", function() {
     openInbox(function(err, box) {
       if (err) throw err;
-      var f = imap.fetch(results, { bodies: "" });
-      f.on("message", function(msg, seqno) {
-        console.log("Message #%d", seqno);
-        var prefix = "(#" + seqno + ") ";
-        msg.on("body", function(stream, info) {
-          console.log(prefix + "Body");
-          simpleParser(stream, { bodies: "" })
-            .then(parsed => {
-              console.log("\n\n\nparsed: ", parsed.text);
-              //step 0 get all email's messageid from backend
-              //step 1 getting all emails
-              //step 2 checking if emails are already in database by messageid
-              //***Either handle comparision with JS or SQL
-              //***SQL has no error handling, but is simpler to do(ANTI JOIN)
-              //***JS can be messed up easily to take longer
-              //step 3 sending all new emails(one at a time) to ds for tagging
-              //step 4 saving data(tags/messageid) ds sends back to database
-              //step 5 adding all tags to emails
-              //step 6 sending all tagged emails to frontend
+      imap.search(["ALL"], function(err, results) {
+        if (err) throw err;
+        var f = imap.fetch(results, { bodies: "" });
+        f.on("message", function(msg, seqno) {
+          console.log("Message #%d", seqno);
+          var prefix = "(#" + seqno + ") ";
+          msg.on("body", function(stream, info) {
+            console.log(prefix + "Body");
+            simpleParser(stream, { bodies: "" })
+              .then(parsed => {
+                console.log("\n\n\nparsed: ", parsed.text);
+                //step 0 get all email's messageid from backend
+                //step 1 getting all emails
+                //step 2 checking if emails are already in database by messageid
+                //***Either handle comparision with JS or SQL
+                //***SQL has no error handling, but is simpler to do(ANTI JOIN)
+                //***JS can be messed up easily to take longer
+                //step 3 sending all new emails(one at a time) to ds for tagging
+                //step 4 saving data(tags/messageid) ds sends back to database
+                //step 5 adding all tags to emails
+                //step 6 sending all tagged emails to frontend
 
-              /* the problem i see with SQL is I don't believe we can pick and choose which 
+                /* the problem i see with SQL is I don't believe we can pick and choose which 
               messageid's (we can't go thru all of them grab the ids and then decide we want to 
               go back to the first message that would have to be another call to open the inbox/
               stream *FROM WHAT I UNDERSTAND*) the only way to do something like this would be to
@@ -91,60 +176,60 @@ route.post("/", (req, res) => {
               be tagged it gets sent to DS when it returns a tag we add to DB and finally add to 
               the array that gets sent to the FE. */
 
-              // oldMessages = [];
-              newMessages = [];
-              for (let emailId of emailsIds) {
-                if (parsed.messageId !== emailId) {
-                  //Sending the new message to DS for tagging
-                  // why is it an http and not axios for tags?
-                  http
-                    .post(
-                      "http://LstmModel-env.4zqtqnkaex.us-east-1.elasticbeanstalk.com/api/tags",
-                      {
-                        sender: parsed.from,
-                        id: parsed.messageId,
-                        subject: parsed.subject,
-                        message: parsed.text
-                      }
-                    )
-                    .then(res => {
-                      let sqlEmailId;
-                      let addEmailObj = {
-                        message_id: parsed.messageId,
-                        user_id: userId
-                      };
-                      Messages.addEmail(addEmailObj).then(id => {
-                        return (sqlEmailId = id);
+                for (let emailId of emailsIds) {
+                  if (parsed.messageId !== emailId) {
+                    //Sending the new message to DS for tagging
+                    // why is it an http and not axios for tags?
+                    http
+                      .post(
+                        "http://LstmModel-env.4zqtqnkaex.us-east-1.elasticbeanstalk.com/api/tags",
+                        {
+                          sender: parsed.from,
+                          id: parsed.messageId,
+                          subject: parsed.subject,
+                          message: parsed.text
+                        }
+                      )
+                      .then(res => {
+                        let sqlEmailId;
+                        let addEmailObj = {
+                          message_id: parsed.messageId,
+                          user_id: userId
+                        };
+                        Messages.addEmail(addEmailObj).then(id => {
+                          return (sqlEmailId = id);
+                        });
+                        let dataTag = res.data.tag;
+                        dataTag.map(tag => {
+                          tag.email_id = sqlEmailId;
+                        });
+                        Tags.addTag(tags);
+                        console.log(parsed.attachments);
+                        // now time to create an object and push it to allMessages array.
+                        // things we will need for this object.  an array of tags the message from
+                        // from messageId subject text attachments
                       });
-                      let dataTag = res.data.tag;
-                      dataTag.map(tag => {
-                        tag.email_id = sqlEmailId;
-                      });
-                      Tags.addTag(tags);
-                    });
-
-                  // let newObj = {};
-                  // newMessages.push(newObj);
-                } else {
+                  } else {
+                    // this is where we will need to pull tags that match the messageid and then
+                    // create the object. read the notes above about object contents and push to allMessages.
+                  }
                 }
-                console.log(parsed.messageId === emailsIds);
-              }
-              // // results to emailsIds.
-              //  seqno.parsed.text
-              // test what results exist in database.  send the nonexisting results to datascience to be tagged.  add tag point to message id that points to user.
-              // console.log(parsed.headers)
-              // console.log(parsed.)
-            })
-            .catch(err => {
-              console.log("\n\n\nerr: ", err);
-            });
-        });
-        //til here
-        msg.once("attributes", function(attrs) {
-          console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
-        });
-        msg.once("end", function() {
-          console.log(prefix + "Finished");
+                // // results to emailsIds.
+                //  seqno.parsed.text
+                // test what results exist in database.  send the nonexisting results to datascience to be tagged.  add tag point to message id that points to user.
+                // console.log(parsed.headers)
+                // console.log(parsed.)
+              })
+              .catch(err => {
+                console.log("\n\n\nerr: ", err);
+              });
+          });
+          msg.once("attributes", function(attrs) {
+            console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
+          });
+          msg.once("end", function() {
+            console.log(prefix + "Finished");
+          });
         });
       });
       f.once("error", function(err) {
@@ -155,6 +240,9 @@ route.post("/", (req, res) => {
         imap.end();
       });
     });
+
+    // this is where we will do res.status() and return the allMessages array for the front end.
+    // the allMessages array is an object of emails.
   });
 
   imap.once("error", function(err) {
