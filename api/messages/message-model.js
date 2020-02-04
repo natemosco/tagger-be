7de70fe4b53ced5280cbb3a76e244e683a079936
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-02-04 13:15:35 -0600

    getTagsFormessage addEmail and findEmailbyId model

--- api/messages/message-model.js
+++ api/messages/message-model.js
@@ -3,14 +3,17 @@ const db = require("../../data/dbConfig.js");
 module.exports = {
   getEmailIds,
   addEmail,
-  deleteAllEmailsByUser
+  deleteAllEmailsByUser,
+  getTagsForMessage,
+  getMessageTags,
+  get
 };
 
 function addEmail(email) {
   return db("emails")
     .insert(email, "id")
     .then(ids => {
-      return [ids];
+      return ids;
     });
 }
 
@@ -24,4 +27,42 @@ function deleteAllEmailsByUser(userId) {
   return db("emails")
     .where("user_id", userId)
     .del();
+}
+
+function getTagsForMessage(messageId) {
+  return db("tags")
+    .select("tags")
+    .where("message_id", messageId);
+}
+
+function getMessageTags() {
+  const messages = db("messages");
+
+  const newMessageArray = messages.map(message => {
+    return get(message.id);
+  });
+  return newMessageArray;
+}
+
+function get(messageId) {
+  const messages = db("messages");
+
+  if (messageId) {
+    messages.where({ messageId }).first();
+
+    const promises = [messages, getTagsForMessage(messageId)];
+
+    return Promise.all(promises).then(results => {
+      const [message, tags] = results;
+
+      if (message) {
+        message.tags = tags;
+
+        return message;
+      } else {
+        return null;
+      }
+    });
+  }
+  return messages;
 }
