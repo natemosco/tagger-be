32cb26c6403c5f7fc85798b62b27c813b5be69d6
Nathaniel Mosco <natemosco@gmail.com>
2020-02-05 18:19:17 -0500

    Co-authored-by: Andrew Wilson <andrew.wilson90@hotmail.com> Co-authored-by: Luis Flores <lflores02142@gmail.com>

--- test/message-router-test.js
+++ test/message-router-test.js
@@ -4,7 +4,7 @@ const server = require("../api/server");
 
 chai.use(chaiHttp);
 
-describe("message-router", function() {
+describe("Message-router", function() {
   it("should return 200", function() {
     chai
       .request(server)
@@ -12,9 +12,10 @@ describe("message-router", function() {
         email: "taggerlabs20@gmail.com",
         //login with above email
         host: "imap.gmail.com",
-        //then use access token located in url bar after the redirect to run this test
+        //token value equivalent to output of below function. Info comes from the return url after redirect when logging into google
+        // btoa(`user=${user}auth=Bearer ${token}`) <-- contains invisible characters when viewed on Mac. Appear as square characters on PC
         token:
-          "ya29.Il-8B4e3sbieYYTQOuhgYdONU2ofL_PT1HKHEffERl0-UIf-DJ2DP7r4j2ZAkKtqRQ4ro1PXOpWn9jfQtOJPpyJOSwauPU0nWxtMJokebYTooGVoXuDhyQYEIOxAlWzfpA"
+          "dXNlcj10YWdnZXJsYWJzMjBAZ21haWwuY29tAWF1dGg9QmVhcmVyIHlhMjkuSWwtOEJ5TVcwRVdSOEwtLXJfcU1OS2VLSERmYWJ4SU1OUHZQRXg5YW9ySnhEWEdnNkMwbHd2eVBVQlNFYkROeUhZYmNFbkZvZGlWblhJeWlHVzROd3U0eXZlVS1YX0dJcEpfMWQwOFdnVmpqWTZ2Y2ZfRU15V1VXR0JqbTV2RjgtZwEB"
       })
       .end(function(err, res) {
         res.chai.should().have.status(200);
