b638c6e13b42a9418d3bb2b36ca9cc1a7142a297
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-02-05 21:24:01 -0600

    remove unused variables combine endpoints /emails

--- api/messages/message-router.js
+++ api/messages/message-router.js
@@ -1,7 +1,6 @@
 // ****** DEPENDENCIES *********
 const router = require("express").Router();
 const axios = require("axios");
-const { google } = require("googleapis");
 require("dotenv").config();
 const rateLimit = require("axios-rate-limit");
 const Imap = require("imap");
@@ -12,99 +11,14 @@ const Messages = require("./message-model");
 const Tags = require("../tags/tag-model");
 
 // ******* GLOBAL VARIABLES **********
-let responseLabels = "";
-let messages = "";
 const http = rateLimit(axios.create(), {
   maxRequests: 1,
   perMilliseconds: 1750
 });
 http.getMaxRPS();
 
-// ********* THE NEW ROUTE WITH IMAP EMAILS WITHOUT TAGS***********
-router.post("/", (req, res) => {
-  const { email, host, token } = req.body;
-  const allMessages = [];
-
-  var imap = new Imap({
-    user: email,
-    password: "",
-    host: host,
-    port: 993,
-    tls: true,
-    xoauth2: token,
-    tlsOptions: { rejectUnauthorized: false },
-    debug: console.log
-  });
-
-  function openInbox(cb) {
-    imap.openBox("INBOX", true, cb);
-  }
-
-  imap.once("ready", function() {
-    openInbox(function(err, box) {
-      if (err) throw err;
-      imap.search(["ALL"], function(err, results) {
-        if (err) throw err;
-        var f = imap.fetch(results, { bodies: "" });
-        f.on("message", function(msg, seqno) {
-          console.log("Message #%d", seqno);
-          var prefix = "(#" + seqno + ") ";
-          msg.on("body", function(stream, info) {
-            console.log(prefix + "Body");
-            simpleParser(stream, { bodies: "" })
-              .then(parsed => {
-                Tags.getTagsByMessageId(parsed.messageId).then(tags => {
-                  let newObj = {
-                    html: parsed.html,
-                    text: parsed.text,
-                    from: parsed.from,
-                    subject: parsed.subject,
-                    attachments: parsed.attachments,
-                    id: parsed.messageId,
-                    tags
-                  };
-                  allMessages.push(newObj);
-                });
-              })
-              .catch(err => {
-                console.log("\n\n\nerr: ", err);
-                res.status(500).json({
-                  message: "Server was unable to help you, good luck!"
-                });
-              });
-          });
-          msg.once("attributes", function(attrs) {
-            console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
-          });
-          msg.once("end", function() {
-            console.log(prefix + "Finished");
-          });
-        });
-        f.once("error", function(err) {
-          console.log("Fetch error: " + err);
-        });
-        f.once("end", function() {
-          console.log("Done fetching all messages!");
-          res.status(200).json(allMessages);
-          imap.end();
-        });
-      });
-    });
-
-    imap.once("error", function(err) {
-      console.log(err);
-    });
-
-    imap.once("end", function() {
-      console.log("Connection ended");
-    });
-  });
-
-  imap.connect();
-});
-
 // ********* THE NEW ROUTE WITH IMAP FOR TAGGING************
-router.post("/tags", (req, res) => {
+router.post("/", (req, res) => {
   const { email, host, token } = req.body;
   const allMessages = [];
 
@@ -150,12 +64,23 @@ router.post("/tags", (req, res) => {
         if (err) throw err;
         var f = imap.fetch(results, { bodies: "", attributes: "" });
         f.on("message", function(msg, seqno) {
-          console.log("Message #%d", seqno);
+          // console.log("Message #%d", seqno);
           var prefix = "(#" + seqno + ") ";
           msg.on("body", function(stream, info) {
-            // console.log(prefix + "Body");
             simpleParser(stream, { bodies: "", attributes: "" })
               .then(parsed => {
+                Tags.getTagsByMessageId(parsed.messageId).then(tags => {
+                  let newObj = {
+                    html: parsed.html,
+                    text: parsed.text,
+                    from: parsed.from,
+                    subject: parsed.subject,
+                    attachments: parsed.attachments,
+                    id: parsed.messageId,
+                    tags
+                  };
+                  allMessages.push(newObj);
+                });
                 const found = emailsIds.includes(parsed.messageId);
                 if (!found) {
                   //Sending the new message to DS for tagging
@@ -179,7 +104,7 @@ router.post("/tags", (req, res) => {
                       Messages.addEmail(addEmailObj).then(message => {
                         sqlEmailId = message.id;
                         let dataTag = res.data.tag;
-                        
+
                         dataTag.forEach(tag => {
                           let newObj = {
                             tag,
@@ -187,34 +112,14 @@ router.post("/tags", (req, res) => {
                           };
                           Tags.addTag(newObj);
                         });
-                        const newObj = {
-                          html: parsed.html,
-                          text: parsed.text,
-                          from: parsed.from,
-                          subject: parsed.subject,
-                          attachments: parsed.attachments,
-                          id: parsed.messageId,
-                          tags: dataTag
-                        };
-                        allMessages.push(newObj);
                       });
                     })
                     .catch(err => {
-                      console.log(err,"Error for posting to DS api for tagging");
+                      console.log(
+                        err,
+                        "Error for posting to DS api for tagging"
+                      );
                     });
-                } else {
-                  Tags.getTagsByMessageId(parsed.messageId).then(tags => {
-                    const newObj = {
-                      html: parsed.html,
-                      text: parsed.text,
-                      from: parsed.from,
-                      subject: parsed.subject,
-                      attachments: parsed.attachments,
-                      id: parsed.messageId,
-                      tags
-                    };
-                    allMessages.push(newObj);
-                  });
                 }
               })
               .catch(err => {
@@ -222,7 +127,7 @@ router.post("/tags", (req, res) => {
               });
           });
           msg.once("attributes", function(attrs) {
-            console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
+            // console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
           });
           msg.once("end", function() {
             console.log(prefix + "Finished");
