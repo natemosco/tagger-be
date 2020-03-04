d717bf81ffa521a2795d48ad0738f86adf4bd221
Vladislav Mogilevskiy <47991120+vladmog@users.noreply.github.com>
2020-03-04 13:36:34 -0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,16 +8,26 @@ const composeRouter = require("./Sender/compose-router")
 
 const server = express();
 
+let whitelist = [
+  "http://localhost:3000", 
+  "https://tagger-lab.netlify.com", 
+  "http://ec2-3-19-30-227.us-east-2.compute.amazonaws.com/"
+]
 // server.use(cors())
-server.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://tagger-lab.netlify.com"] }));
+server.use(cors({ 
+  credentials: true, 
+  origin: whitelist, 
+  allowedHeaders: ["Origin, X-Requested-With, Content-Type, Accept, Set-Cookie, Authorization"],
+  methods: ["POST"]
+}));
 
 
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie, Authorization");
-  res.header('Access-Control-Allow-Credentials', true)
-  next();
-});
+// server.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie, Authorization");
+//   res.header('Access-Control-Allow-Credentials', true)
+//   next();
+// });
 
 server.use(helmet());
 server.use(express.json());
